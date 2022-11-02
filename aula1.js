6e303cd18967e152ba7573a5d86e10a642d9845a
FF857 <eufelipe857@gmail.com>
2022-11-02 16:39:59 -0300

    add conteudo

---
+++ aula1.js
@@ -0,0 +1,6 @@
+
+let variavel = 10;
+
+
+console.log("teste");
+console.log(variavel);
---
+++ desafio_1.js
@@ -0,0 +1,13 @@
+
+/* ir de canoas-rs até torres-rs */
+
+let combustivel_l = 4.91;
+let km_por_l= 11;
+let distancia_Km = 195;
+
+let l_consumo = distancia_Km / km_por_l;
+
+let resultado = l_consumo * combustivel_l;
+
+console.log(resultado.toFixed(2));
+
