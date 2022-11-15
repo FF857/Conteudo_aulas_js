fadbfd16957d0f406abeb84ae8a42fa23bea3105
FF857 <eufelipe857@gmail.com>
2022-11-15 14:44:43 -0300

    atualização desafio_5

--- desafio_5.js
+++ desafio_5.js
@@ -1,28 +1,43 @@
-/*  TIPO DE PAGAMENTO:
+/*  
+    TIPO DE PAGAMENTO:
     cartão de debito = 1
     dinheiro ou pix = 2
     duas vezes = 3
+    mais de duas = 4
 */
 
-function aplicaJuros(valorProduto, parcelaProduto, tipoPagamento){
-    if(parcelaProduto == 1 && tipoPagamento == 1){
-        return (valorProduto - ((valorProduto * 10)/100));
-        /*desconto = ((valorProduto * 10) / 100);
-        valorProduto = (valorProduto - desconto);
-        console.log(valorProduto);*/
-    } else if(parcelaProduto == 1 && tipoPagamento == 2){
-        desconto = ((valorProduto * 15) / 100);
-        valorProduto = (valorProduto - desconto);
-        console.log(valorProduto);
-    }else if(parcelaProduto == 2 && tipoPagamento == 3){
-        console.log(valorProduto);
-    } else {
-        juros = ((valorProduto * 10) / 100);
-        valorProduto = (valorProduto + juros);
-        console.log(valorProduto)
-    }
-
+function aplicaDesconto(valorProduto, valorDesconto){
+    return (valorProduto - ((valorProduto * valorDesconto) / 100));
+}
+function aplicaJuros(valorProduto, valorJuros){
+    return (valorProduto + ((valorProduto * valorJuros) / 100));
 }
 
-aplicaJuros(100, 1, 1);
+var valorEtiqueta = 100;
+var tipoPagamento = 4;
 
+
+if(tipoPagamento == 1){
+
+    valorEtiqueta = aplicaDesconto(valorEtiqueta, 10);
+    console.log(valorEtiqueta);
+
+} else if(tipoPagamento == 2){
+
+    valorEtiqueta = aplicaDesconto(valorEtiqueta, 15);
+    console.log(valorEtiqueta);
+
+} else if(tipoPagamento == 3){
+
+    console.log(valorEtiqueta);
+
+} else if (tipoPagamento == 4){
+
+    valorEtiqueta = aplicaJuros(valorEtiqueta, 10);
+    console.log(valorEtiqueta);
+
+}else {
+
+    console.log("Tipo de pagamento incorreto!");
+    
+}
