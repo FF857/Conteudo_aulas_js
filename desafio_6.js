80036abcf77cce7ef9117643eb070089b6a49416
FF857 <eufelipe857@gmail.com>
2022-12-08 00:03:49 -0300

    add conteudo

--- desafio_6.js
+++ desafio_6.js
@@ -22,5 +22,6 @@ class Carro{
 }
 
 const uno = new Carro("Fiat", "Preto", 1 / 12);
-
 console.log(uno.calcularGastoDePercurso(70, 5));
+const polo = new Carro("volkswagen", "Azul", 1 /10);
+console.log(polo.calcularGastoDePercurso(70, 5))
