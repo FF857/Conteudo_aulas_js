530761df70995f429e61a5b7a4a756c91ac45f98
FF857 <eufelipe857@gmail.com>
2022-12-07 23:14:26 -0300

    add conteudo

---
+++ desafio_6.js
@@ -0,0 +1,26 @@
+
+class Carro{
+
+    modelo;
+    cor;
+    gastoMedioPorKm;
+
+    constructor (marca, cor, gastoMedioPorKm){
+
+        this.marca = marca;
+        this.cor = cor;
+        this.gastoMedioPorKm = gastoMedioPorKm;
+
+    }
+
+    calcularGastoDePercurso (distanciaEmKm, precoCombustivel){
+
+        return distanciaEmKm * this.gastoMedioPorKm * precoCombustivel;
+
+    }
+    
+}
+
+const uno = new Carro("Fiat", "Preto", 1 / 12);
+
+console.log(uno.calcularGastoDePercurso(70, 5));
